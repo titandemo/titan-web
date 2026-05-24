c1e351078db0c95f5fdeac26bd6a38505e48b662
titandemo <3227764+emdeeeks@users.noreply.github.com>
2026-05-24 09:53:41 +0100

    Totally unnecessary, yet wanted sine-scroller

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,31 @@
+function buildScroller(text) {
+  const chars = text.toUpperCase().split('');
+  return chars.map(function(ch) {
+    const code = ch.charCodeAt(0);
+    if (code >= 65 && code <= 90) {
+      const letter = String.fromCharCode(code).toLowerCase();
+      return '<span class="pf pf-' + letter + '"></span>';
+    }
+    return '<span class="space"></span>';
+  }).join('');
+}
+
 $(document).ready(function() {
   $.get('data.yaml')
     .done(function(yamlText) {
       try {
         const data = jsyaml.load(yamlText);
 
+        if (data.scroller) {
+          var html = buildScroller(data.scroller);
+          $('.sine-scroller').html(html);
+        }
+
         const $news = $('#news');
         $news.empty();
 
         $.each(data.news, function(index, thing) {
-            $news.append(`<li class="list-group-item border-0 ps-0">${thing}</li>`);
+            $news.append('<li class="list-group-item border-0 ps-0">' + thing + '</li>');
         });
 
       } catch (error) {
